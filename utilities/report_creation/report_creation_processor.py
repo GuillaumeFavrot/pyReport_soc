47270f94b4c281babf59d00235a4e9b12ceeed45
Guillaume Favrot <gui.favrot@gmail.com>
2024-05-23 15:58:30 +0200

    corrections and addition of input checking errors to log

--- utilities/report_creation/report_creation_processor.py
+++ utilities/report_creation/report_creation_processor.py
@@ -9,7 +9,7 @@ def create_report(timer:Timer, logger:Logger, template_name: str, report_name: s
     logger.info(f'{timer.get_time()} - Création du rapport {report_name} à partir du modèle {template_name}')
 
     # Vérification du type des variables template_name et report_name
-    if type(template_name | report_name) != str:
+    if type(template_name) != str or type(report_name) != str:
         logger.error(f'{timer.get_time()} - Les noms des fichiers "report_name" et "template_name" doivent être des chaînes de caractères')
         raise TypeError('"report_name" and "template_name" file name must be strings')
 
@@ -19,7 +19,7 @@ def create_report(timer:Timer, logger:Logger, template_name: str, report_name: s
     # Ouverture du fichier source
     source_file =  open(template_file_path, 'rb')
 
-    # Ouverture du fichier desistantion
+    # Ouverture du fichier de destination
     destination_file = open(report_file_path, 'wb')
 
     # use the shutil.copyobj() method to copy the contents of source_file to destination_file
